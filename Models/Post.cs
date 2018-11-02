56b48e510fd028b44a2e443651d814e18c49be61
Benjamin Orinoloye <bjfikky@yahoo.com>
2018-11-02 09:25:57 -0400

    Add Post Model

---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+    }
+}
