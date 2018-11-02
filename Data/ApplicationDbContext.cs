f736021b826f39f44a8869c23b6a1252b3bc4ef5
Benjamin Orinoloye <bjfikky@yahoo.com>
2018-11-02 09:27:08 -0400

    Added Post to DBset

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Blog.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace Blog.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<Post> Posts { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
