b0b8233341e8b9369a467b541903fa070d85d54e
Benjamin Orinoloye <bjfikky@yahoo.com>
2018-11-06 11:04:52 -0500

    Added PostController and Post view

---
+++ Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Add() 
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Post post)
+        {
+            post.DateTime = DateTime.Now;
+            
+            return View();
+        }
+    }
+}
