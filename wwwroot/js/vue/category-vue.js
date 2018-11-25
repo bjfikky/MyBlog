60418e8dd08cd20852ec26d468670c4a00c94c60
Benjamin Orinoloye <bjfikky@yahoo.com>
2018-11-25 15:16:51 -0500

    Used vue modal for add category

--- wwwroot/js/vue/category-vue.js
+++ wwwroot/js/vue/category-vue.js
@@ -1,12 +1,28 @@
-﻿new Vue ({
+﻿let modal = Vue.component('modal', {
+    template: '#modal-template'
+  })
+
+new Vue ({
     el: '#category',
+
+    components: {modal: modal},
+
     data: {
+        showModal: false,
+        
         categories: [
             {id : 1, name : 'C#'},
             {id : 2, name : 'Java'},
             {id : 3, name : 'PHP'},
             {id : 4, name : 'Python'},
             {id : 5, name : 'JavaScript'},
-        ]
+        ],
+
+        
+ 
+    },
+
+    methods: {
+
     }
 })
